c85d039efeab87d195751774000dc51fdc049d2c
HeliumApple <58273279+HeliumApple@users.noreply.github.com>
2020-08-24 17:03:53 -0400

    exemple project

---
+++ pc_testing/src/pc_testing/henshinChange.java
@@ -0,0 +1,93 @@
+package pc_testing;
+import org.eclipse.emf.henshin.interpreter.EGraph;
+import org.eclipse.emf.henshin.interpreter.Engine;
+import org.eclipse.emf.henshin.interpreter.UnitApplication;
+import org.eclipse.emf.henshin.interpreter.impl.EGraphImpl;
+import org.eclipse.emf.henshin.interpreter.impl.EngineImpl;
+import org.eclipse.emf.henshin.interpreter.impl.UnitApplicationImpl;
+import org.eclipse.emf.henshin.model.Module;
+import org.eclipse.emf.henshin.model.Rule;
+import org.eclipse.emf.henshin.model.Unit;
+import org.eclipse.emf.henshin.model.resource.HenshinResourceSet;
+import org.eclipse.emf.ecore.EDataType;
+
+import org.logicng.datastructures.Tristate;
+import org.logicng.formulas.FormulaFactory;
+import org.logicng.io.parsers.ParserException;
+import org.logicng.io.parsers.PropositionalParser;
+import org.logicng.solvers.MiniSat;
+import org.logicng.solvers.SATSolver;
+
+public class henshinChange {
+
+	public static void main(String[] args) {
+		String path = "model/";
+		// Create a resource set with a base directory:
+		HenshinResourceSet resourceSet = new HenshinResourceSet(path);
+				
+		// Load the module:
+		Module module = resourceSet.getModule("OhMy.henshin", false);
+
+		// Load the example model into an EGraph:
+		EGraph graph = new EGraphImpl(resourceSet.getResource("SpaceModel.xmi"));
+		//EGraph graph = new EGraphImpl(resourceSet.getResource("example-bank.xmi"));
+				
+		// Create an engine and a rule application:
+		Engine engine = new EngineImpl();
+		engine.LoadAndWrite("model/spl_test.csv", "model/hope.csv");
+		UnitApplication jebediah = new UnitApplicationImpl(engine);
+		jebediah.setEGraph(graph);
+		jebediah.setEGraph(graph);
+		System.out.println("Jebediah shall not crash THIS rocket!!!");
+		jebediah.setUnit(module.getUnit("jebediah"));
+		//jebediah.setParameterValue("Hope", "(A & B)");
+		//jebediah.execute(null);
+		System.out.println();
+		
+		UnitApplication Bob = new UnitApplicationImpl(engine);
+		Bob.setEGraph(graph);
+		Bob.setEGraph(graph);
+		System.out.println("Bob shall not crash THIS rocket!!!");
+		Bob.setUnit(module.getUnit("addbob"));
+		
+		/*for (Rule r: module.getAllRules()) {
+			
+		}*/
+		
+		//module.getUnit("addbob").setLifted(true);
+		//jebediah.setParameterValue("halp", "stuff");
+		
+		
+		UnitApplication Bill = new UnitApplicationImpl(engine);
+		Bill.setEGraph(graph);
+		Bill.setEGraph(graph);
+		System.out.println("Bill shall not crash THIS rocket!!!");
+		System.out.println("\n\n");
+		
+		Bill.setUnit(module.getUnit("deletebill"));
+		//module.getUnit("deletebill").setLifted(false);
+		//module.getUnit("jebediah").setLifted(false);
+		//module.getUnit("addbob").setLifted(true);
+		
+		System.out.println("bob");
+		if (!Bob.execute(null)) {
+			throw new RuntimeException("Error creating account for Bob");
+		}
+		
+		/*System.out.println("jeb");
+		if (!jebediah.execute(null)) {
+			throw new RuntimeException("Error creating account for jeb");
+		}*/
+		
+		/*System.out.println("Bill");
+		if (!Bill.execute(null)) {
+			throw new RuntimeException("Error creating account for bill");
+		}*/
+		//System.out.println(graph.getRoots().get(0));
+		resourceSet.saveEObject(graph.getRoots().get(0), "SpaceModel1_result.xmi");
+		System.out.println("\nSaved result file.");
+		//engine.Set_Return_File("model/hopingsecond.csv");
+		//engine.LoadAndWrite("model/spl_test.csv", "model/hopingsecond.csv");
+		//Bob.execute(null);
+	}
+}
